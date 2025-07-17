4fa411225a13c7f9f5a366b7eebd82bf026bfdf2
C1616 <brian.chanda@ymail.com>
2025-07-17 15:01:59 +0200

    ...

--- obj/Debug/net8.0/LocalGov360.AssemblyInfo.cs
+++ obj/Debug/net8.0/LocalGov360.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocalGov360")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6d2362f7c047d3a1a01ece63bd65dbb5a3198a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c3dd3837bc8c795ccad47da4606e79269e9a7fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocalGov360")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocalGov360")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
