0323b8ecc33e86bdd9232b385fd92288698ea13c
C1616 <brian.chanda@ymail.com>
2025-07-25 06:43:42 +0200

    Payment Config

---
+++ Data/Migrations/20250724094650_AlterInitiatedBy.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocalGov360.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterInitiatedBy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InitiatedBy",
+                table: "WorkflowInstances");
+
+            migrationBuilder.AddColumn<string>(
+                name: "InitiatedById",
+                table: "WorkflowInstances",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkflowInstances_InitiatedById",
+                table: "WorkflowInstances",
+                column: "InitiatedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkflowInstances_Users_InitiatedById",
+                table: "WorkflowInstances",
+                column: "InitiatedById",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkflowInstances_Users_InitiatedById",
+                table: "WorkflowInstances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WorkflowInstances_InitiatedById",
+                table: "WorkflowInstances");
+
+            migrationBuilder.DropColumn(
+                name: "InitiatedById",
+                table: "WorkflowInstances");
+
+            migrationBuilder.AddColumn<string>(
+                name: "InitiatedBy",
+                table: "WorkflowInstances",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ Data/Migrations/20250724134110_UseWorkflowInstanceIdInstead.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocalGov360.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseWorkflowInstanceIdInstead : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "ServiceSubmissions");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "WorkflowInstanceId",
+                table: "ServiceSubmissions",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WorkflowInstanceId",
+                table: "ServiceSubmissions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "ServiceSubmissions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
